f418e56f98175ff1592b12678237e0dbd8592700
hpym365 <hpym365@gmail.com>
2017-06-06 14:07:24 +0800

    data handle

---
+++ queueDataHandle/src/test/java/com/senyint/test/TestServ.java
@@ -0,0 +1,97 @@
+package com.senyint.test;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.senyint.squeue.DataHandleApplication;
+import com.senyint.squeue.demo.service.TestService;
+import com.senyint.squeue.queue.dao.QueueDao;
+import com.senyint.squeue.queue.service.QueueService;
+import com.senyint.squeue.result.Result;
+import com.senyint.squeue.utils.ResultUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author hpym365@gmail.com
+ * @Description
+ * @Date 17-6-5 下午1:59
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DataHandleApplication.class)
+public class TestServ {
+//    @Autowired
+//    TestService serv;
+//
+//    @Test
+//    public void right() {
+//        serv.right();
+//    }
+//
+//    @Test
+//    public void wrong() {
+//        serv.wrong();
+//    }
+
+    @Autowired
+    QueueService queueService;
+
+
+    @Test
+    public void selectqueue() {
+        Map map = new HashMap();
+        map.put("token", "abcd");
+
+        List queueByToken = queueService.findQueueByToken(map);
+        System.out.println(queueByToken);
+    }
+
+
+    @Test
+    public void insertQueue() {
+        Map map = new HashMap();
+        map.put("queueNum", "dfdfa");
+        map.put("queueName", "dfdf");
+        map.put("queueDesc", "dfdf");
+        map.put("deptId", "1");
+
+        queueService.insertQueue(map);
+    }
+
+
+    @Autowired
+    private RestTemplate rest;
+
+
+    @Test
+    public void findQueueByToken() throws IOException {
+        String token = "abcd";
+        Map param = new HashMap();
+        param.put("token",token);
+        //        map.put("queueNum", "fs-cs-fubu-A");
+//        map.put("patientName", "王小二");
+//        map.put("queueDate", LocalDate.now());
+//        map.put("optionType", 1);//1-增  2-删 3-改 4-查
+
+
+        String res = rest.postForObject("http://localhost:9090/data/findQueueByToken", param, String.class);
+        System.out.println(res);
+
+//        Map<String, Object> map = new HashMap<String, Object>();
+        Result result = ResultUtil.getResult(res);
+        System.out.println(result.toString());
+//        rest.getForObject("http://localhost:8888/receive/" + token,Map.class);
+    }
+
+
+}
